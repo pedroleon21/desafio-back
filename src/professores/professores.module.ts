711a97d8d3b8703407e93f7fd001b400e9730f4c
Pedro Leon Paranayba Clerot <paranayba.clerot@gmail.com>
2022-09-28 20:29:06 -0300

    import do DataBaseModule e professorPRovides

--- src/professores/professores.module.ts
+++ src/professores/professores.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProfessoresService } from './professores.service';
 import { ProfessoresController } from './professores.controller';
+import { professorProvider } from 'src/repositoty/reposytory.profesor';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [ProfessoresController],
-  providers: [ProfessoresService],
+  providers: [...professorProvider,
+    ProfessoresService],
 })
 export class ProfessoresModule { }
