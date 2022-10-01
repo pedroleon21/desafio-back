d90fff2a3e6a16e82d08c0f6ea1a1c2d502ca779
Pedro Leon Paranayba Clerot <paranayba.clerot@gmail.com>
2022-10-01 16:10:43 -0300

    concluindo pacote service.materia.td

--- src/materia/materia.service.ts
+++ src/materia/materia.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, Inject, HttpStatus } from '@nestjs/common';
-import { HttpException } from '@nestjs/common/exceptions/http.exception';
 import { ServideFailureException } from 'src/exceptions/servide.failure.exception';
 import { Repository } from 'typeorm';
 import { CreateMateriaDto } from './dto/create-materia.dto';
@@ -8,10 +7,10 @@ import { Materia } from './entities/materia.entity';
 
 @Injectable()
 export class MateriaService {
-  constructor(@Inject(repository_names.MATERIA_REPOSITORY)
+  constructor(@Inject(REPOSITORY_NAMES.MATERIA_REPOSITORY)
   private reposytory: Repository<Materia>) { }
   create(createMateriaDto: CreateMateriaDto) {
-    return 'This action adds a new materia';
+    return this.reposytory.save(this.reposytory.create(createMateriaDto));
   }
 
   findAll(): Promise<Materia[]> {
@@ -25,9 +24,9 @@ export class MateriaService {
   async update(id: number, updateMateriaDto: UpdateMateriaDto): Promise<Materia> {
     await this.reposytory
       .createQueryBuilder()
-      .update(UpdateMateriaDto)
-      .where({ id })
-      .execute
+      .update(updateMateriaDto)
+      .where("materia.id = :id ", { id })
+      .execute();
     return this.reposytory.findOneBy({ id });
   }
 
