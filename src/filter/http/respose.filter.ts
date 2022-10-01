0e549c636b7757bacd38022a3d205b8406eb3f67
Pedro Leon Paranayba Clerot <paranayba.clerot@gmail.com>
2022-10-01 16:03:35 -0300

    console log na exception

--- src/filter/http/respose.filter.ts
+++ src/filter/http/respose.filter.ts
@@ -8,6 +8,7 @@ export class HttpExceptionFilter implements ExceptionFilter {
         const response = ctx.getResponse<Response>();
         const request = ctx.getRequest<Request>();
         const status = exception.getStatus();
+        console.error(exception);
         response
             .status(status)
             .json({
