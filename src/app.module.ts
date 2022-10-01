e1bfe6f23fd1b9e68fb3f17f7139ad13b66b165e
Pedro Leon Paranayba Clerot <paranayba.clerot@gmail.com>
2022-10-01 20:04:45 -0300

    importe dos modulos de user a Auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,17 @@ import { CoffeeModule } from './coffee/coffee.module';
 import { ProfessoresModule } from './professores/professores.module';
 import { MateriaModule } from './materia/materia.module';
 import { AulaModule } from './aula/aula.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     AulaModule,
     ProfessoresModule,
     CoffeeModule,
-    MateriaModule
+    MateriaModule,
+    AuthModule,
+    UsersModule
   ],
 })
 export class AppModule { }
