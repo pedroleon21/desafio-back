742320cb2a3a393e81d3ae652f0d3f813387582a
Pedro Leon Paranayba Clerot <paranayba.clerot@gmail.com>
2022-10-01 16:00:14 -0300

    mudandodata para string

--- src/materia/entities/materia.entity.ts
+++ src/materia/entities/materia.entity.ts
@@ -1,10 +1,14 @@
-import { Column, PrimaryGeneratedColumn } from "typeorm";
+import { Aula } from "src/aula/entities/aula.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
+@Entity()
 export class Materia {
     @PrimaryGeneratedColumn()
     id: number
-    @Column()
-    LocalDate: Date;
-    @Column({ length: 280 })
+    @Column({ length: 280, })
     conteudo: string
+    @Column({ length: 50 })
+    materiaName: string
+    @OneToMany(() => Aula, aula => aula.materia)
+    aulas: Aula[]
 }
